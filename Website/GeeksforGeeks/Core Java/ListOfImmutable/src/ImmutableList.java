4650585d2fd2b4c9658cff67a8368150943ab745
shshetudev <shshetudev@gmail.com>
2019-10-15 18:33:41 +0600

    sdfs

---
+++ Website/GeeksforGeeks/Core Java/ListOfImmutable/src/ImmutableList.java
@@ -0,0 +1,13 @@
+import static java.util.regex.Grapheme.T;
+
+public class ImmutableList {
+    /*
+    * 1.Create a function to create ImmutableList
+    * 2.Print the ImmutableMap
+    * 3.call the function in the main method
+    * */
+public static void iList(){
+    public static <T> void iList(List<T> iList){}
+}
+
+}
